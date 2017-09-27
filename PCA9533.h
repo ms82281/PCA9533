c78dea97bfa244a67e558d26346978f25cad9138
Max Schmid <max@schmid-live.de>
2017-09-27 14:32:45 +0200

    minor update

--- PCA9533.h
+++ PCA9533.h
@@ -14,6 +14,7 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 #if 1
 __asm volatile ("nop");
 #endif
